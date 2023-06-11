04e5c902950ec0fba9632ac95da388f4453f64fd
Michael Ong <michael.ong@constructor.io>
2023-06-11 10:47:14 +0800

    upgrade to next 13

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "@chakra-ui/react";
-
-export default function Header({ title }) {
-  return (
-    <Text fontSize={16} color="green.100" fontWeight={"black"}>
-      {title}
-    </Text>
-  );
-}
